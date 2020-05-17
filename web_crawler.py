93fb8d2de8c9e7acd14992e024a6658e91402e9f
Chewing <caac1008@gmail.com>
2020-05-17 21:34:03 +0800

    更新web_crawler.py

--- web_crawler.py
+++ web_crawler.py
@@ -1,48 +1,94 @@
-import pandas as pd
-import re
+#資料清理用
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
+from datetime import datetime
+#匯入資料庫
+import mysql.connector
+from mysql.connector import Error
+import pymysql
 
-#建立表格,有4個欄位:分類(category)、名稱(name)、排行(rank)、超連結(hyperlink)
-df = pd.DataFrame({'category': [], 'name': [], 'rank': [], 'hyperlink': []})
-pattern = 'https:\/\/www\.books\.com\.tw\/web\/(.*)'#category
-
-#https://www.books.com.tw/robots.txt來看可用的User-Agent
+categoryDic = {}
+subCategoryDic = {}
+today = datetime.today().strftime('%Y-%m-%d')
+#取得用robots.txt來看可用的User-Agent
 headers = {
-    'User-Agent': '*', 
-    'From': 'caac21008@gmail.com'#E-mail
+    'User-Agent': '*'
+    #'From': ''#E-mail
 }
-#粗體字分類
-r = requests.get('https://www.books.com.tw/web/sys_categoryIndex', headers = headers, allow_redirects = True)
-soup = BeautifulSoup(r.text, 'html.parser')
-category_list = soup.find_all("div", "mod type03_m008 clearfix")
 
-#尋找該分類的前10暢銷品,並將資訊加入df
+r = requests.get('https://www.books.com.tw/web/sys_categoryIndex', headers=headers, allow_redirects=True)
+soup = BeautifulSoup(r.text, 'html.parser')
+category_list = soup.find_all("div","mod type03_m008 clearfix")
+df = pd.DataFrame({'category' : [], 'item_name' : [], 'ranking' : [], 'hyperlink' : [], 'update_date': []})
+
 def category_bestseller(url, df):
-    r_category = requests.get(url, headers = headers, allow_redirects = True)
+    r_category = requests.get(url, headers=headers,allow_redirects=True)
     soup = BeautifulSoup(r_category.text, 'html.parser')
-    category_list = soup.find_all("ul", "number_a")
+    category_list = soup.find_all("ul","number_a")
     count = 1
-    #不是每一分類都有熱門排行
     if category_list != []:
         for item in category_list:
             for i in range(1,len(item.find_all("a"))):
-                new_row = {'category': re.match(pattern, url).group(1), 'name': item.find_all("a")[i].text, 'rank': count, 'hyperlink': item.find_all("a")[i]['href']}
-                df = df.append(new_row, ignore_index = True)
+                new_row = {'category':categoryDic[url], 'item_name':item.find_all("a")[i].text, 'ranking':f'{count:.0f}', 'hyperlink':item.find_all("a")[i]['href'], 'update_date': today}
+                df = df.append(new_row, ignore_index=True)
                 count = count + 1
     return df
 
-#透過超連結找出分類,並透過category_bestseller尋找該分類的前10暢銷品
+#大分類,利用category_bestseller尋找該分類的前10暢銷品
 for i in range(1, len(category_list)):
     if category_list[i].find('a') == None:
         continue
+    categoryDic[category_list[i].find('a')['href']] = category_list[i].find('a').text
     df = category_bestseller(category_list[i].find('a')['href'], df)
 
-#觀察df,因magazine與fbooks資訊與books、china、mooks不同,將兩分類刪除
-df.drop(df.loc[df['category']=='magazine'].index, inplace = True)
-df.drop(df.loc[df['category']=='fbooks'].index, inplace = True)
-df.reset_index(drop = True, inplace = True)
+#不是每一分類都有熱門排行,就算找得到也要看內容才能確定是否正確,將找出不正確資訊的分類刪除   
+df.drop(df.loc[df['category']=='magazine'].index, inplace=True)
+df.drop(df.loc[df['category']=='fbooks'].index, inplace=True)
+df.drop(df.loc[df['category']=='雜誌'].index, inplace=True)
+df.drop(df.loc[df['category']=='外文書'].index, inplace=True)
+df.reset_index(drop=True, inplace=True)
 
-#輸出
-df.to_csv("博客來排行.csv", index = False)
-df.to_excel('博客來排行.xls', index = False)
+#中文書的細類,利用category_bestseller尋找該分類的前10暢銷品
+categoryDic = {}
+df_sub = pd.DataFrame({'category' : [], 'item_name' : [], 'ranking' : [], 'hyperlink' : [], 'update_date': []})
+r_category = requests.get('https://www.books.com.tw/web/books/', headers=headers,allow_redirects=True)
+
+soup = BeautifulSoup(r_category.text, 'html.parser')
+category_list = soup.find("div","mod_b type02_l001-1 clearfix").find_all("a")
+
+for i in range(0, len(category_list)):
+    categoryDic[category_list[i]['href']] = category_list[i].text
+    df_sub = category_bestseller(category_list[i]['href'], df_sub)
+    
+try:
+    #連接 MySQL資料庫
+    connection = mysql.connector.connect(
+        host = "127.0.0.1",
+        user = "python_user",
+        password = "python123",
+        database = "mynewdb",
+        use_pure = True)
+
+    #將匯入資料庫
+    cursor = connection.cursor()
+
+    for index, row  in df.iterrows():
+        sql = "INSERT INTO TB_RANKING(CATEGORY, ITEM_NAME, RANKING, HYPERLINK, UPDATE_DATE) VALUES (%s, %s, %s, %s, %s)"
+        cursor.execute(sql, tuple(row))
+    connection.commit()
+    
+    for index, row  in df_sub.iterrows():
+        sql = "INSERT INTO TB_SUBCATEGORY(CATEGORY, ITEM_NAME, RANKING, HYPERLINK, UPDATE_DATE) VALUES (%s, %s, %s, %s, %s)"
+        cursor.execute(sql, tuple(row))
+    connection.commit()
+    print("資料庫匯入已完成")
+
+except Error as e:
+    print("資料庫連接失敗：", e)
+
+finally:
+    if (connection.is_connected()):
+        cursor.close()
+        connection.close()
+        print("資料庫連線已關閉")
